c0989d71c42623ffe9e7abe6883e127c4d2a7402
sevenjames <sevenjames@users.noreply.github.com>
2020-03-05 13:06:17 -0600

    Update stick_mouse.py
    Converted comment blocks to doc-strings.

--- stick_mouse.py
+++ stick_mouse.py
@@ -1,19 +1,21 @@
-# FreePIE script for mouse cursor control with gamepad stick.
-# Specifically: right stick x axis for semi-absolute mouse cursor control.
-# Can be modified for any analog axis for absolute or relative movement.
-# 2020-03-02 JAO
+"""
+FreePIE script for mouse cursor control with gamepad stick.
+Specifically: right stick x axis for semi-absolute mouse cursor control.
+Can be modified for any analog axis for absolute or relative movement.
+2020-03-02 JAO
 
-# (!) FreePIE scripts are automatically looped as fast as possible; too fast.
-# This is managed with a "starting" block and a frame limit timer.
-# See documentation here: https://github.com/AndersMalmgren/FreePIE
+(!) FreePIE scripts are automatically looped as fast as possible; too fast.
+This is managed with a "starting" block and a frame limit timer.
+See documentation here: https://github.com/AndersMalmgren/FreePIE
 
-# inputs reference for gamepad : Logitech F310
-# left stick LR = X axis, left stick UD = Y axis
-# right stick LR = X rotation, right stick UD = Y rotation
-# left trigger = +Z axis, right trigger = -Z axis
+inputs reference for gamepad : Logitech F310
+left stick LR = X axis, left stick UD = Y axis
+right stick LR = X rotation, right stick UD = Y rotation
+left trigger = +Z axis, right trigger = -Z axis
+"""
 
 if starting:
-	# this section only runs once
+	"""This section only runs once."""
 	import time
 	j_id = 0 # gamepad device number
 	jrx_mult = 0.45 # movement range for absolute mode
@@ -21,17 +23,19 @@ if starting:
 	jrx_dead = 3 # dead zone for relative mode to prevent drifting
 
 def main_abs_mode():
-	# semi-absolute mode: stick deflection sets mouse cursor position
-	# is only semi because "origin" is based on cursor position, not screen
-	# mouse cursor will return to "origin" when stick springs back to center
+	"""Semi-absolute mode: stick deflection sets mouse cursor position.
+	Is only semi because "origin" is based on cursor position, not screen.
+	Mouse cursor will return to "origin" when stick springs back to center.
+	"""
 	jrx_in = joystick[j_id].xRotation
 	jrx_delta = filters.delta(jrx_in)
 	jrx_out = jrx_delta * jrx_mult
 	mouse.deltaX = jrx_out
 
 def main_rel_mode():
-	# relative mode: stick deflection sets mouse cursor speed
-	# mouse cursor speed will return to zero when stick springs back to center
+	"""Relative mode: stick deflection sets mouse cursor speed.
+	Mouse cursor speed will return to zero when stick springs back to center.
+	"""
 	jrx_in = joystick[j_id].xRotation
 	jrx_map = filters.mapRange(jrx_in, -1000, 1000, -jrx_sens, jrx_sens)
 	jrx_out = filters.deadband(jrx_map, jrx_dead)
